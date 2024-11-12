0f3f5e26fc7357036ad99f5cc311afd5aae58483
bog <119977760+aukak@users.noreply.github.com>
2024-11-12 16:35:21 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare:'https://bare.ubg.icu/bare/',
+    bare:'hhttps://getto.masplenedigitalworld.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
