88b802f3614e8db4d0ea671d8ce5fdbcca09cd00
bog <119977760+aukak@users.noreply.github.com>
2024-11-12 22:52:05 -0600

    Delete loading.js

--- loading.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.style.opacity = '0';
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-    }, 500);
-};
