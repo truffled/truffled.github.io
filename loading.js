658cf885bb45ec82b6f1e400e0658769e657e336
bog <maxminges3@gmail.com>
2024-11-13 00:27:23 -0600

    fiixed loading

--- loading.js
+++ loading.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.transition = 'opacity 1s'; 
     loadingScreen.style.opacity = '0';
     setTimeout(() => {
         loadingScreen.style.display = 'none';
-    }, 500);
+    }, 2000); 
 };
