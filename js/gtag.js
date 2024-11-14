379f27dbfbfb3ae14743289793ab669fc973c135
bog <maxminges3@gmail.com>
2024-11-14 00:24:59 -0600

    banana

--- js/gtag.js
+++ js/gtag.js
@@ -3,7 +3,8 @@
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
+  
   gtag('js', new Date());
-
+  
   gtag('config', 'G-WLTG1S4STB');
 </script>
